adcff7e39e9cec5d453beaea5c20c02b18b7415e
Jeffry Samuel <69224322+Jeffser@users.noreply.github.com>
2023-09-21 19:02:13 -0600

    Delete config directory

--- config/waybar/scripts/ytmv.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import os, json
-title = os.popen("playerctl metadata title").read().replace(" - YouTube Music", "").replace(" - YouTube", "").replace("\n", "").replace('&', '&amp;')
-print(json.dumps({"text": f"󰎈 {title}" if title != "" else ""}))
